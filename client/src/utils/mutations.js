9b8b7227cb5784cc77ceccf1b0edb9a1e1969225
ry brown <ryan.alex.brown@gmail.com>
2021-01-07 22:00:24 -0700

    fix

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,7 +44,7 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($input: savedBook!) {
+ mutation saveBook($input: bookInput!) {
     saveBook(input: $input) {
       _id
       username
