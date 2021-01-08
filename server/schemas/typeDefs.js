cce047bcd1f3da2659a2e95c9f9ad47a73ed9441
ry brown <ryan.alex.brown@gmail.com>
2021-01-07 18:42:59 -0700

    trying again

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -37,7 +37,7 @@ type Query{
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: bookInput): User
+    saveBook(input: savedBook!): User
     removeBook(bookId: String!): User
 }
 
